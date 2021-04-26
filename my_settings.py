7ef2b716c7081420756e87d285f3c73c25f29917
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-04-26 16:20:41 +0900

    Add: 초기세팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-@-ygfui-b39uf&bj5^(y1d)ex3%)9+a&xkjrp%72ksj7a#*z5u'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'herhouse',
+        'USER': 'root',
+        'PASSWORD': 'adsf25did',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
