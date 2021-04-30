80060bb30e848d5f1c53456d9c480c0deb2f4b54
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-04-30 12:40:22 +0900

    Modify:카테고리,제품 리스트 표출 유닛테스트 및 오타 수정

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from products.views import CategoryView 
+from products.views import CategoryView, ProductListView
 
 urlpatterns = [
-    path('/category', CategoryView.as_view())
+    path('/category', CategoryView.as_view()),
+    path('', ProductListView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,17 +1,51 @@
 import json
 
-from django.http  import JsonResponse
-from django.views import View
+from django.http            import JsonResponse
+from django.views           import View
+from django.db.models       import Q, Avg
+from django.core.exceptions import FieldError
 
-from products.models import Category
+from products.models import Category, Product
 
 class CategoryView(View):
     def get(self,request):
         category_lists = [
             {
-            category_id : category.id,
-            name        : category.name,
-            image_url   : category.image_url
+            'category_id' : category.id,
+            'name'        : category.name,
+            'image_url'   : category.image_url
             }
         for category in Category.objects.all()]        
         return JsonResponse({'MESSAGE':'SUCCESS','category_lists':category_lists}, status=200)
+
+class ProductListView(View):
+    def get(self,request):
+        try: 
+            ordering    = request.GET.get('ordering','-price')
+            category_id = request.GET.get('category-id', None)
+
+            q=Q()
+            if category_id:
+                q = Q(category_id=category_id)
+            products = Product.objects.filter(q).annotate(star_rating=Avg('review__star_rating')).order_by(ordering)
+            
+            if len(products)==0:
+                return JsonResponse({'MESSAGE':'INVALID_CATEGORY_ID'}, status=401)
+            
+            product_lists = [
+                {
+                    'id'              : product.id,
+                    'name'            : product.name,
+                    'price'           : product.price,
+                    'manufacturer'    : product.manufacturer,
+                    'discount_rate'   : product.discount_rate,
+                    'star_rating'     : round(product.review_set.all().aggregate(Avg('star_rating'))['star_rating__avg'],1),
+                    'review_number'   : product.review_set.count(),
+                    'is_freedelivery' : product.is_freedelivery,
+                    'thumbnail_image' : product.thumbnail_image
+                } for product in products]
+            
+            return JsonResponse({'MESSAGE':'SUCCESS','product_lists':product_lists}, status=200)
+        
+        except FieldError:
+            return JsonResponse({'MESSAGE':'INVALID_ORDERING_METHOD'}, status=401)
