bd376b296944554299ec2ae9c7d0e824ba14d9ec
fcfargo <fcfargo90@gmail.com>
2021-04-30 16:52:59 +0900

    [19기 김영훈] Refactor: 소셜로그인 유닛테스트 멘토 리뷰 반영

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,247 @@
-from django.test import TestCase
+import bcrypt
+import jwt
+import json
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from django.test   import Client, TestCase
+
+from users.models  import User
+import my_settings
+
+class KakaoSignInTest(TestCase): 
+    def setUp(self):
+        password          = '1234'
+        hashed_password   = bcrypt.hashpw(
+                password.encode('UTF-8'), bcrypt.gensalt()
+            ).decode('UTF-8')
+        User.objects.create(
+            name          = '침착맨',
+            email         = 'test@gmail.com',
+            password      = hashed_password,
+            phone_number  = '01012341111',
+           )
+
+    def tearDown(self):
+        User.objects.filter(name='침착맨').delete()
+
+    def test_key_error(self):
+        c = Client()
+        header = {}
+        
+        response = c.post('/users/kakao-signin', content_type='applications/json', **header)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "KEY_ERROR",
+                }
+            )
+
+    @patch('users.views.requests') 
+    def test_kakao_signin_invalid_token(self, mocked_request):
+
+        class FakeResponse:
+            def json(self):
+                return {
+                }
+        
+        mocked_request.get = MagicMock(return_value = FakeResponse())
+        
+        c = Client()
+        header = {'HTTP_Authorization':'fake_token.1234'}
+
+        response = c.post('/users/kakao-signin', content_type='applications/json', **header)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "INVALID_TOKEN",
+                }
+            )
+
+    @patch('users.views.requests') 
+    def test_kakao_signin_invalid_user(self, mocked_request):
+
+        class FakeResponse:
+            def json(self):
+                return {
+                    "id" : 123456,
+                    "kakao_account": {"email":"test@naver.com"}
+                }
+
+        mocked_request.get = MagicMock(return_value = FakeResponse())
+        
+        c = Client()
+        header = {'HTTP_Authorization':'fake_token.1234'}
+        
+        response = c.post('/users/kakao-signin', content_type='applications/json', **header)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "INVALID_USER",
+                    "id_number" : 123456,
+                    "email"     : "test@naver.com",
+                }
+            )
+
+    @patch('users.views.requests') 
+    def test_kakao_signin_success(self, mocked_request):
+
+        class FakeResponse:
+            def json(self):
+                return {
+                    "id" : 123456,
+                    "kakao_account": {"email":"test@gmail.com"}
+                }
+        
+        mocked_request.get = MagicMock(return_value = FakeResponse())
+        token = jwt.encode({
+                'user_id':User.objects.get(email="test@gmail.com").id}, 
+                my_settings.SECRET_KEY, algorithm="HS256"
+            )
+
+        c = Client()
+        header = {'HTTP_Authorization':'fake_token.1234'}
+        
+        response = c.post('/users/kakao-signin', content_type='applications/json', **header)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+                {
+                    "message" : "SUCCESS",
+                    "token"   : token,
+                }
+            )
+if __name__ == '__main__':
+    unittest.main()
+
+class KakaoSignupTest(TestCase): 
+    def test_email_error(self):
+        c = Client()
+        params = {
+            "email"        : "turkymp3naver.com",
+            "id_number"    : 123456,
+            "name"         : "주펄린",
+            "phone_number" : "01033332222",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "EMAIL_ERROR",
+                }
+            )
+    def test_id_number_error(self):
+        c = Client()
+        params = {
+            "email"        : "turkymp3@naver.com",
+            "id_number"    : "abcd",
+            "name"         : "주펄린",
+            "phone_number" : "01033332222",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "ID_NUMBER_ERROR",
+                }
+            )
+    def test_name_error(self):
+        c = Client()
+        params = {
+            "email"        : "turkymp3@naver.com",
+            "id_number"    : 123456,
+            "name"         : "",
+            "phone_number" : "01033332222",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "NAME_ERROR",
+                }
+            )
+    def test_phone_number_error(self):
+        c = Client()
+        params = {
+            "email"        : "turkymp3@naver.com",
+            "id_number"    : 123456,
+            "name"         : "주펄린",
+            "phone_number" : "0103333",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "PHONE_NUMBER_ERROR",
+                }
+            )
+
+    def test_key_error(self):
+        c = Client()
+        params = {
+            "email"       : "turkymp3@naver.com",
+            "id_number"    : 123456,
+            "name"         : "주펄린",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "KEY_ERROR",
+                }
+            )
+
+    def test_json_decoder_error(self):
+        c = Client()
+        params = {
+            "email"        : "turkymp3@naver.com",
+            "id_number"    : 123456,
+            "name"         : "주펄린",
+            "phone_number" : "01033333233",
+        }
+        
+        response = c.post('/users/kakao-signup', params, content_type='applications/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "Json_Decode_Error",
+                }
+            )
+
+    @patch('users.views.requests') 
+    def test_kakao_signup_success(self, mocked_request):
+        c = Client()
+        params = {
+            "email"        : "turkymp3@naver.com",
+            "id_number"    : 123456,
+            "name"         : "주펄린",
+            "phone_number" : "01033333233",
+        }
+        
+        response = c.post('/users/kakao-signup', json.dumps(params), content_type='applications/json')
+        token = jwt.encode({
+                'user_id':User.objects.get(email="turkymp3@naver.com").id}, 
+                my_settings.SECRET_KEY, algorithm="HS256"
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+                {
+                    "message"   : "SUCCESS",
+                    "token"     : token
+                }
+            )
+if __name__ == '__main__':
+    unittest.main()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+import bcrypt
+import json
+import jwt
+import requests
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.views           import View
+
+from .models import User
+import my_settings
+
+class KakaoSigninView(View):
+    def post(self, request):
+        try:
+            access_token    = request.headers['Authorization']
+
+            profile_request = requests.get(
+                    "https://kapi.kakao.com/v2/user/me", 
+                    headers={"Authorization" : f"Bearer {access_token}"}
+            ).json()
+
+            if profile_request.get('code')==-401 or not profile_request:
+                return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 401)
+
+            id_number       = profile_request.get('id')
+            email           = profile_request.get('kakao_account').get('email')
+
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'message' : 'INVALID_USER', 'id_number' : id_number, 'email' : email }, status=401)
+
+            token = jwt.encode({
+                'user_id':User.objects.filter(email=email).first().id}, 
+                my_settings.SECRET_KEY, algorithm="HS256"
+            )
+            return JsonResponse({'message' : 'SUCCESS', 'token' : token}, status=200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+        
+class KakaoSignupView(View):
+    def post(self, request):     
+        try:
+            data = json.loads(request.body)
+            email         = data['email']
+            id_number     = str(data['id_number'])
+            name          = data['name']
+            phone_number  = data['phone_number']
+
+            if not my_settings.EMAIL_CHECK.match(email): 
+                return JsonResponse({"message" : "EMAIL_ERROR"}, status=400)
+
+            if not my_settings.ID_NUMBER_CHECK.match(id_number): 
+                return JsonResponse({"message" : "ID_NUMBER_ERROR"}, status=400)
+
+            if not my_settings.NAME_CHECK.match(name): 
+                return JsonResponse({"message" : "NAME_ERROR"}, status=400)
+
+            if not my_settings.PHONE_NUMBER_CHECK.match(phone_number): 
+                return JsonResponse({"message" : "PHONE_NUMBER_ERROR"}, status=400)
+
+            hashed_password = bcrypt.hashpw(id_number.encode('UTF-8'), bcrypt.gensalt()).decode('UTF-8')
+
+            user = User(
+                email        = email,
+                password     = hashed_password,
+                name         = name,
+                phone_number = phone_number,
+            )
+            user.save()
+
+            token = jwt.encode({'user_id': user.id}, my_settings.SECRET_KEY, algorithm="HS256")      
+            return JsonResponse({"message" : "SUCCESS", "token" : token}, status = 200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"message" : "Json_Decode_Error"}, status = 400)
