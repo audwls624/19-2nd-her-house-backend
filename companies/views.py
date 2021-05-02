e2d3ce96f88b3352ada1c1a3c9b7cd32f83224f0
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-05-02 17:06:45 +0900

    Refactor: filter 기능 수정 - 필요없는 조건 부분 삭제

--- companies/views.py
+++ companies/views.py
@@ -12,15 +12,17 @@ from my_settings  import API_KEY
 
 class CompanyMainView(View):
     def get(self, request):
-        location = request.GET.get('location', None)
-        
+        location   = request.GET.get('location', None)
+        DEF_RADIUS = 5.0
+        radius     = request.GET.get('radius', DEF_RADIUS)
+
         if location:
             url      = 'https://dapi.kakao.com/v2/local/search/address.json?&query=' + location
             result   = requests.get(urlparse(url).geturl(), headers={'Authorization': f'KakaoAK {API_KEY}'}).json()
             match    = result['documents'][0]['address']
             user_lat = float(match['y'])
             user_lng = float(match['x'])
-            
+
         user_location = (user_lat, user_lng)
         
         company = [{
@@ -35,4 +37,6 @@ class CompanyMainView(View):
             'images'           : [i.image_url for i in company.companyimage_set.all()]}
             for company in Company.objects.all().prefetch_related('companyimage_set')]
         
+        company = [x for x in company if x['distance'] <= float(radius)]
+
         return JsonResponse({'MESSAGE':company}, status=200)
