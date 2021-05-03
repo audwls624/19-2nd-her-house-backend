b9884dff4743d40088fc2947a431b0c064798be6
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-05-03 20:32:34 +0900

    Modify: 제품 상세정보 유닛테스트 url 수정

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from products.views import CategoryView, ProductListView
+from products.views import CategoryView, ProductListView, ProductDetailView
 
 urlpatterns = [
     path('/category', CategoryView.as_view()),
     path('', ProductListView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
 ]
