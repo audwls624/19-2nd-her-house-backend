f64f5ef19db9d7538e56885fd340ba3104b76a81
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-05-05 11:55:46 +0900

    Modify:제품리뷰 생성,표출 view 수정

--- products/tests.py
+++ products/tests.py
@@ -1,12 +1,23 @@
-from django.test import TestCase, Client
-from products.models import Category, Product, Review, Size, Color, ProductOption, ProductImage
-from users.models import User
-
 import json
+import boto3
+import jwt
+import unittest
+
+from unittest.mock import patch, MagicMock
+
+from django.test        import TestCase, Client
+from django.core.files  import File
+
+from products.models  import Category, Product, Review, Size, Color, ProductOption, ProductImage
+from users.models     import User
+
+from my_settings import SECRET_KEY
+
+
 
 
 class CategoryViewTest(TestCase):
-    
+
     @classmethod
     def setUpTestData(cls):
         Category.objects.create(name='침',image_url='https://un.com')
@@ -88,22 +99,21 @@ class ProductListView(TestCase):
             'MESSAGE': 'SUCCESS',
             'product_lists': [
                 {'id': 4, 'name': '가4', 'price': '1400.00',
-                 'manufacturer': 'f', 'discount_rate': '0.40',
-                 'star_rating': '3.2', 'review_number': 4, 'is_freedelivery': False,
-                 'thumbnail_image': 'https://ima.com'},
+                    'manufacturer': 'f', 'discount_rate': '0.40',
+                    'star_rating': '3.2', 'review_number': 4, 'is_freedelivery': False,
+                    'thumbnail_image': 'https://ima.com'},
                 {'id': 3, 'name': '가3', 'price': '1300.00',
-                 'manufacturer': 'f', 'discount_rate': '0.30',
-                 'star_rating': '2.0', 'review_number': 4, 'is_freedelivery': True,
-                 'thumbnail_image': 'https://ima.com'},
+                    'manufacturer': 'f', 'discount_rate': '0.30',
+                    'star_rating': '2.0', 'review_number': 4, 'is_freedelivery': True,
+                    'thumbnail_image': 'https://ima.com'},
                 {'id': 2, 'name': '가2', 'price': '1200.00',
-                 'manufacturer': 'sf', 'discount_rate': '0.20',
-                 'star_rating': '2.8', 'review_number': 4, 'is_freedelivery': False,
-                 'thumbnail_image': 'https://ima.com'},
+                    'manufacturer': 'sf', 'discount_rate': '0.20',
+                    'star_rating': '2.8', 'review_number': 4, 'is_freedelivery': False,
+                    'thumbnail_image': 'https://ima.com'},
                 {'id': 1, 'name': '가1', 'price': '1100.00',
-                 'manufacturer': '소', 'discount_rate': '0.10',
-                 'star_rating': '2.2', 'review_number': 4, 'is_freedelivery': True,
-                 'thumbnail_image': 'https://ima.com'}]
-                 })
+                    'manufacturer': '소', 'discount_rate': '0.10',
+                    'star_rating': '2.2', 'review_number': 4, 'is_freedelivery': True,
+                    'thumbnail_image': 'https://ima.com'}]})
         
     def test_invalid_category_id(self):
         client=Client()
@@ -161,6 +171,7 @@ class ProductDetailViewTest(TestCase):
                 'name': '가구1',
                 'star_rating': '2.333333',
                 'review_number': 3,
+                'price': '1100.00',
                 'discout_rate': '0.10',
                 'delivery_fee': '500.00',
                 'delivery_method': True,
@@ -176,3 +187,53 @@ class ProductDetailViewTest(TestCase):
         response = client.get('/store/4')
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(), {'MESSAGE': 'PRODUCT_ID_DOES_NOT_EXISTS'})
+
+class ProductReviewTest(TestCase):
+
+    def setUp(self):
+        a  = Category.objects.create(id=1,name='침실/가구',image_url='https://ima.com')
+        p1 = Product.objects.create(id=1,name='가구1',price=1100,discount_rate=0.1,is_freedelivery=True,
+            delivery_fee=500,delivery_method=True,thumbnail_image='https://ima.com',category=a,
+            manufacturer='다이소',description='aaaa',description_image='https://ima.com')
+        Product.objects.create(id=2,name='가4',price=1400,discount_rate=0.4,
+            is_freedelivery=False,delivery_fee=500,delivery_method=True,thumbnail_image='https://ima.com',
+            category_id=1,manufacturer='f',description='찬f',description_image='https://ima.com')
+        u1 = User.objects.create(id=1,name='1',email='fda@naver.com',password='fjd',phone_number='010-1234-4567')
+        Review.objects.create(user=u1,product=p1,star_rating=5,image_url='https://i.com',text='fd')
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        Review.objects.all().delete()
+
+    @patch('products.views.boto3.client')
+    def test_review_upload(self, mock_s3client):
+        client                       = Client()
+        mock_file                    = MagicMock(spec=File)
+        mock_file.name               = '101.jpg'
+        mock_s3client.upload_fileobj = MagicMock()
+        access_token                 = jwt.encode({'user_id': 1}, SECRET_KEY, algorithm='HS256')
+        headers                      = {'HTTP_Authorization': access_token}
+        form_data                    = {'filename': mock_file,'json':json.dumps({'star_rating':3,'text':'aa'})}
+        response = client.post('/store/review/1', form_data, **headers)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'MESSAGE': 'SUCCESS'})
+
+    def test_review_show(self):
+        client=Client()
+        response = client.get('/store/review/1')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {
+            'MESSAGE': 'SUCCESS',
+            'Review_lists': [
+                {'user': '1',
+                    'star_rating': '5.00',
+                    'text': 'fd',
+                    'image_url': 'https://i.com'}]
+                    })
+    
+    def test_review_invalid_product(self):
+        client = Client()
+        response = client.get('/store/review/5')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(),{'MESSAGE':'INVALID_PRODUCT_ID'})
--- products/views.py
+++ products/views.py
@@ -1,11 +1,15 @@
 import json
+import boto3
+import random
+import my_settings
 
 from django.http            import JsonResponse
 from django.views           import View
 from django.db.models       import Q, Avg
-from django.core.exceptions import FieldError
+from django.core.exceptions import FieldError, ObjectDoesNotExist
 
-from products.models import Category, Product
+from products.models  import Product, Category, Review
+from users.decorators import login_required
 
 class CategoryView(View):
     def get(self,request):
@@ -29,24 +33,24 @@ class ProductListView(View):
                 q = Q(category_id=category_id)
             products = Product.objects.filter(q).annotate(star_rating=Avg('review__star_rating')).order_by(ordering)
             
-            if len(products)==0:
-                return JsonResponse({'MESSAGE':'INVALID_CATEGORY_ID'}, status=401)
-            
             product_lists = [
                 {
-                    'id'              : product.id,
-                    'name'            : product.name,
-                    'price'           : product.price,
-                    'manufacturer'    : product.manufacturer,
-                    'discount_rate'   : product.discount_rate,
-                    'star_rating'     : round(product.review_set.all().aggregate(Avg('star_rating'))['star_rating__avg'],1),
-                    'review_number'   : product.review_set.count(),
-                    'is_freedelivery' : product.is_freedelivery,
-                    'thumbnail_image' : product.thumbnail_image
+                    'id'                  : product.id,
+                    'name'                : product.name,
+                    'price'               : product.price,
+                    'manufacturer'        : product.manufacturer,
+                    'discount_rate'       : product.discount_rate,
+                    'star_rating'         : round(product.review_set.all().aggregate(Avg('star_rating'))['star_rating__avg'],1) if product.review_set.all() else None,
+                    'review_number'       : product.review_set.count(),
+                    'is_freedelivery'     : product.is_freedelivery,
+                    'thumbnail_image'     : product.thumbnail_image,
+                    'hot_deal'            : random.randrange(0,2), 
                 } for product in products]
             
+            if len(product_lists)==0:
+                return JsonResponse({'MESSAGE':'INVALID_CATEGORY_ID'}, status=401)
+            
             return JsonResponse({'MESSAGE':'SUCCESS','product_lists':product_lists}, status=200)
-        
         except FieldError:
             return JsonResponse({'MESSAGE':'INVALID_ORDERING_METHOD'}, status=401)
 
@@ -59,13 +63,14 @@ class ProductDetailView(View):
                 'category'          : product.category.name,
                 'manufacturer'      : product.manufacturer,
                 'name'              : product.name,
-                'star_rating'       : product.review_set.all().aggregate(Avg('star_rating')).get('star_rating__avg', None),
+                'star_rating'       : product.review_set.all().aggregate(Avg('star_rating')).get('star_rating__avg', None) if product.review_set.all() else None,
                 'review_number'     : product.review_set.count(),
+                'price'             : product.price,
                 'discout_rate'      : product.discount_rate,
                 'delivery_fee'      : product.delivery_fee,
                 'delivery_method'   : product.delivery_method,
-                'size'              : [size.name for size in product.size.all()],
-                'color'             : [color.name for color in product.color.all()],
+                'size'              : list(set([size.name for size in product.size.all()])),
+                'color'             : list(set([color.name for color in product.color.all()])),
                 'thumbnail_image'   : product.thumbnail_image,
                 'description'       : product.description,
                 'description_image' : product.description_image,
@@ -74,3 +79,50 @@ class ProductDetailView(View):
             return JsonResponse({'MESSAGE':'SUCCESS','product_info':product_info}, status=200)
         except Product.DoesNotExist:
             return JsonResponse({'MESSAGE':'PRODUCT_ID_DOES_NOT_EXISTS'}, status=401)
+
+class ProductReviewView(View):
+    
+    @login_required
+    def post(self, request, product_id):
+        data        = json.loads(request.POST.get('json'))
+        file        = request.FILES.get('filename')
+        star_rating = data.get('star_rating')
+        text        = data.get('text')
+        
+        self.s3_client = boto3.client(
+            's3',
+            aws_access_key_id = my_settings.AWS_S3_ACCESS_KEY_ID,
+            aws_secret_access_key = my_settings.AWS_S3_SECRET_ACCESS_KEY
+        )
+        
+        self.s3_client.upload_fileobj(
+            file, 
+            my_settings.AWS_STORAGE_BUCKET_NAME,
+            file.name,
+            ExtraArgs={
+                "ContentType": file.content_type
+            }
+        )
+        if file:
+            file_endpoint = str(file.name).replace(' ','+')
+            image_url     = f'https://herhousebucket.s3.ap-northeast-2.amazonaws.com/{file_endpoint}'
+        
+        Review.objects.create(star_rating  = star_rating,
+                                text       = text,
+                                product_id = product_id,
+                                user_id    = request.user.id,
+                                image_url  = image_url if file else None)
+
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.prefetch_related('review_set','user').get(id = product_id)
+            review_lists = [{
+                'user' : review.user.name,
+                'star_rating' : review.star_rating,
+                'text' : review.text,
+                'image_url' : review.image_url} for review in product.review_set.all()] if product.review_set.all() else None     
+            return JsonResponse({'MESSAGE':'SUCCESS', 'Review_lists': review_lists}, status=201)
+        except Product.DoesNotExist:
+            return JsonResponse({'MESSAGE':'INVALID_PRODUCT_ID'}, status=401)
