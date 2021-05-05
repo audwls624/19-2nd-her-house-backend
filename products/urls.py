571a8c0d0ff1ecc2c243784794871cc2b557516d
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-05-05 11:55:46 +0900

    Modify:제품리뷰 생성,표출 view 수정

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from products.views import CategoryView, ProductListView, ProductDetailView
+from products.views import CategoryView, ProductListView, ProductDetailView, ProductReviewView
 
 urlpatterns = [
     path('/category', CategoryView.as_view()),
     path('', ProductListView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/review/<int:product_id>', ProductReviewView.as_view())
 ]
